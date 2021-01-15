f9d51b21be0c319907112db788b3395721dffe91
SHAH DHRUVI SUDHIR <75307389+ShahDhruvi29@users.noreply.github.com>
2021-01-15 19:15:20 +0530

    Add files via upload

---
+++ js/divisions.js
@@ -0,0 +1,20 @@
+class Divisions {
+    constructor(x, y, w, h) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        strokeWeight(5)
+        stroke("pink")
+        fill(245,25,45);
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
